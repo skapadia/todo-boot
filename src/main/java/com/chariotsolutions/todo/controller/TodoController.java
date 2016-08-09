6fd35ccf139b4135738fd459e7fecbf48dc90659
skapadia <skapadia@chariotsolutions.com>
2016-08-09 09:22:44 -0400

    Adds a CounterService to TodoController that tracks request metrics and also manually increments a counter for calls to /todo/list

--- src/main/java/com/chariotsolutions/todo/controller/TodoController.java
+++ src/main/java/com/chariotsolutions/todo/controller/TodoController.java
@@ -6,6 +6,7 @@ import com.chariotsolutions.todo.repository.TodoItemRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.metrics.CounterService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -20,6 +21,9 @@ public class TodoController {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
+    private CounterService counter;
+
+    @Autowired
     private TodoItemRepository itemRepo;
 
     @RequestMapping(method = RequestMethod.GET)
@@ -30,6 +34,7 @@ public class TodoController {
 
     @RequestMapping(value = "/list", method = RequestMethod.GET)
     public List<TodoItem> findAll() {
+        counter.increment("controllers.todo.list.requests.count");
         return itemRepo.findAll();
     }
 }
