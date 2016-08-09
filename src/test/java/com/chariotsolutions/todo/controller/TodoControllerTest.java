cc8d940932d1e2cc9f72af4cfb3021a71bdd86e1
skapadia <skapadia@chariotsolutions.com>
2016-08-09 07:48:30 -0400

    Adds test/resources directory with configuration; Adds a repository integration test, using main SpringApplication for bean configuration

--- src/test/java/com/chariotsolutions/todo/controller/TodoControllerTest.java
+++ src/test/java/com/chariotsolutions/todo/controller/TodoControllerTest.java
@@ -1,14 +1,13 @@
 package com.chariotsolutions.todo.controller;
 
 
-import com.chariotsolutions.todo.controller.TodoController;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockServletContext;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.mock.web.MockServletContext;;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -18,7 +17,7 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 @SpringBootTest(classes = MockServletContext.class)
 @WebAppConfiguration
 public class TodoControllerTest {
---
+++ src/test/java/com/chariotsolutions/todo/repository/TodoItemRepositoryTest.java
@@ -0,0 +1,35 @@
+package com.chariotsolutions.todo.repository;
+
+import com.chariotsolutions.todo.app.Application;
+import com.chariotsolutions.todo.model.TodoItem;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+public class TodoItemRepositoryTest {
+
+    @Autowired
+    private TodoItemRepository itemRepo;
+
+    @Before
+    public void setUp() {
+        itemRepo.deleteAll();
+        itemRepo.insert(new TodoItem("Pay mortgage bill", LocalDateTime.now()));
+
+    }
+
+    @Test
+    public void testFindAll() {
+        List<TodoItem> items = itemRepo.findAll();
+        assert(items.size() == 1);
+    }
+
+}
